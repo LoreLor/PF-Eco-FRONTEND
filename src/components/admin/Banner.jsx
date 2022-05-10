56198ae75e9c3f3ab4dd8624ffc7a6b6c68e4940
SebastianUrquijo <sebastian.urquijo98@gmail.com>
2022-05-10 17:49:38 -0500

    test commit

--- src/components/admin/Banner.jsx
+++ src/components/admin/Banner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Banner.module.css"
 export default function CategoryBanner({children, isOpen,setIsOpen}){
+    //test
     function handleClick(e){
         e.stopPropagation()
     }
