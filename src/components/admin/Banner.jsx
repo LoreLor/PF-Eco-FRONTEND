711d91ae952f15bb00bef5faba59f7d171762e1b
SebastianUrquijo <sebastian.urquijo98@gmail.com>
2022-05-10 18:02:42 -0500

    forzado

--- src/components/admin/Banner.jsx
+++ src/components/admin/Banner.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./Banner.module.css"
 export default function CategoryBanner({children, isOpen,setIsOpen}){
-    //test
     function handleClick(e){
         e.stopPropagation()
     }
+    //Prueba jodida
     return(
         <div className={`${styles.banner} ${isOpen && styles.show}`} onClick={()=> setIsOpen(state =>!state)}>
             <div className={styles["modal-content"]} onClick={handleClick}>
