071bc5c6638bd13faba596917b3954ea542577aa
Gabriel Navas <cgnavas98@gmail.com>
2022-05-12 16:52:18 -0300

    router ready

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import ProductAdmin from './components/admin/productForm/ProductAdmin'
 import UserAdmin from './components/admin/userAdmin/UserAdmin'
 import {Routes,Route} from 'react-router-dom'
 import Login from './components/login/Login';
+import ProductDetail from './components/detalleProducto/ProductDetail';
 
 function App() {
   return (
     <Fragment>
       <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}/>
         <Route path='/admin' element={<AdminPage/>}/>
         <Route path='/userAdmin' element={<UserAdmin/>}/>
         <Route path='/productAdmin' element={<ProductAdmin/>}/>
         <Route path ='/categoryAdmin' element={<CategoryAdmin/>}/>
         <Route path ='/login' element={<Login />} />
+        <Route path = '/home/:id' element={<ProductDetail/>}/>
       </Routes>
     </Fragment>
   );
---
+++ src/components/detalleProducto/ProductDetail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ProductDetail (){
+    return(
+        <h1>soy el detail</h1>
+    )
+}
