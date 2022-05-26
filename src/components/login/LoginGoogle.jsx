34e5a41bab7392caf0e162f4c22d83b26a2471ea
Lorena De Armas <lorenadearmas01@gmail.com>
2022-05-26 12:50:45 -0300

    config_back

--- src/components/login/LoginGoogle.jsx
+++ src/components/login/LoginGoogle.jsx
@@ -1,10 +1,25 @@
 import { GoogleLogin } from "@react-oauth/google";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import * as queryString from "query-string";
 
+const queryParams = queryString.stringify({
+    client_id: 'CLIENT_ID_FROM_THE_APP', // It must correspond to what we declared earlier in the backend
+    scope: "email", // This is the user data you have access to, in our case its just the mail.
+    redirect_uri: redirectUri, // This is the uri that will be redirected to if the user signs into his google account successfully
+    auth_type: "rerequest", // This tells the consent screen to reappear if the user initially entered wrong credentials into the google modal
+    display: "popup", 
+    response_type: "code" // This tells Google to append code to the response which will be sent to the backend which exchange the code for a token
+});
+const url = `https://accounts.google.com/o/oauth2/v2/auth?${queryParams}`;
+
+render () {
+...
+<a href={url}>Continue with Google</a>
+}
 
 function LoginGoogle() {
   const navigate = useNavigate();
+  
 
   return (
       <GoogleLogin
--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ import { Provider } from 'react-redux';
 import store, {Persistor} from './redux/store/index';
 import { PersistGate } from 'redux-persist/integration/react';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId="74065711880-a66epk147cn7qohac76h2s5fq8qpqpsm.apps.googleusercontent.com">
       <PayPalScriptProvider options={{ "client-id": "AaQI-1adqVEH1wWgNAa9IlvPvWf4rOLo5-zttK1nrlMMxkU1WBUo0zSmPfWpKVWaBTL3TpwiQl1dEaDQ"}}>
         <Provider store={store}>
           <BrowserRouter>
@@ -21,6 +23,7 @@ root.render(
           </BrowserRouter>   
         </Provider>
       </PayPalScriptProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
