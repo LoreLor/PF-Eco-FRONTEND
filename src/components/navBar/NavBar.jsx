975320c1c479f9b1b13febc443dbc78d6dbd66f4
LoreLor <lorenadearmas01@gmail.com>
2022-05-31 17:25:19 -0300

    badge-finalizado

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -15,26 +15,21 @@ import Badge from '@mui/material/Badge';
 export default function NavBar({ categories, paginado }) {
     const user = useSelector((state)=>state.users.userInfo)
 
-    // const cartUser = useSelector((state) => state.products.cart)
+    const cartUser = useSelector((state) => state.products.cart)
     // console.log('cartUser :>> ', cartUser);
-     //const cartGuest = useSelector((state) => state.products.cartGuest)
-    
-      //const cart = user && user.id? cartUser.details : cartGuest
-    
-     // const qty = cart.reduce((a, c) => a + c.bundle, 0)
+    const cartGuest = useSelector((state) => state.products.cartGuest) 
+    const cart = user && user.id? cartUser.details : cartGuest
+    const qty = cart && [].concat(cart).reduce((a, c) => a + c.bundle, 0)
 
 // console.log('cartbun :>> ', cart);
-    useEffect(() => {
-        dispatch(getCart())
-        dispatch(getCartGuest())
-    }, [])
-
-
     const dispatch = useDispatch();
-    // function handleCart(e){
-    //     e.preventDefault()
-    //     alert('carrito')
-    // }
+
+    useEffect(()=>{
+        dispatch(getCart(cartUser.id))
+
+    },[])
+
+    // 
 
     function handleLogout() {
         dispatch(logout())
@@ -144,7 +139,7 @@ export default function NavBar({ categories, paginado }) {
                     <NavLink to="/cart">
 
                         <button className="btn btn-secundary" type="button" style={{ marginRight: '10px' }}>
-                            <Badge badgeContent={7}  color="secondary">
+                            <Badge badgeContent={qty}  color="secondary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#F66B0E" className="bi bi-cart-check" viewBox="0 0 16 16">
                                 <path d="M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
